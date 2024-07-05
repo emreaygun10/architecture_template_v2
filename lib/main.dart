45510061806e479f85c1b26c7fb6f334e09a6d71
Emre Aygün <emreaygun1157@hotmail.com>
2024-07-05 16:01:42 +0300

    app initilialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:architecture_template/feature/home/view/home_view.dart';
+import 'package:architecture_template/product/init/application_initialize.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  ApplicationInitialize().make();
   runApp(ProductLocalization(child: const MyApp()));
 }
 
---
+++ lib/product/init/application_initialize.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_logger/easy_logger.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kartal/kartal.dart';
+import 'package:logger/logger.dart';
+
+@immutable
+
+/// This class is used to initialize the application process
+final class ApplicationInitialize {
+  /// It's only use for setup business
+  Future<void> make() async {
+    await runZonedGuarded<Future<void>>(_initialize, (error, stack) {
+      Logger().e(error);
+    });
+  }
+
+  /// This method is used to initialize the application process
+  static Future<void> _initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await EasyLocalization.ensureInitialized();
+
+    EasyLocalization.logger.enableLevels = [LevelMessages.error];
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    ///TODO: SPLASH
+    await DeviceUtility.instance.initPackageInfo();
+
+    FlutterError.onError = (details) {
+      ///crashlytics log insert here
+      ///custom service or custom logger insert here
+      Logger().e(details.exceptionAsString());
+    };
+  }
+}
