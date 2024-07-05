0c19603ae19bba30b4dfe1b14b5dac69b15f2b37
Emre Aygün <emreaygun1157@hotmail.com>
2024-07-05 15:42:30 +0300

    add localization

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,7 +1,13 @@
 import 'package:architecture_template/feature/home/view/mixin/home_view_mixin.dart';
+import 'package:architecture_template/product/init/language/locale_keys.g.dart';
+import 'package:architecture_template/product/init/product_localization.dart';
+import 'package:architecture_template/product/utility/constants/enums/locales.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
 
 part 'widget/home_app_bar.dart';
+
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -9,9 +15,29 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> with HomeViewMixin{
+class _HomeViewState extends State<HomeView> with HomeViewMixin {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: _HomeAppBar(),
+      body: Container(
+        height: context.sized.height,
+        width: context.sized.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                ProductLocalization.updateLanguage(
+                  context: context,
+                  value: Locales.tr,
+                );
+              },
+              child: const Text(LocaleKeys.general_buttons_save).tr(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
