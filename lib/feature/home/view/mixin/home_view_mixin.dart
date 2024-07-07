a25bcf767a3b0bc29bad9dd7d80e73e2dd9b2df6
Emre Aygün <emreaygun1157@hotmail.com>
2024-07-07 18:10:47 +0300

    vexana

--- lib/feature/home/view/mixin/home_view_mixin.dart
+++ lib/feature/home/view/mixin/home_view_mixin.dart
@@ -1,6 +1,20 @@
 import 'package:architecture_template/feature/home/view/home_view.dart';
+import 'package:architecture_template/product/navigation/app_router.dart';
+import 'package:architecture_template/product/service/manager/product_network_manager.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
-mixin HomeViewMixin on State<HomeView>{
+mixin HomeViewMixin on State<HomeView> {
+  late final ProductNetworkManager productNetworkManager;
 
+  @override
+  void initState() {
+    super.initState();
+    productNetworkManager = ProductNetworkManager.base();
+    productNetworkManager.listenErrorState(
+      onErrorStatus: (value) {
+        context.router.push(HomeRoute());
+      },
+    );
+  }
 }
